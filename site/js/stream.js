db4d8be779eac255106a92046506ffd79984c935
Jérôme Velociter <jerome@velociter.fr>
2014-10-11 22:20:02 +0200

    IT'S WORKING
    (kind of)

--- site/js/stream.js
+++ site/js/stream.js
@@ -2,5 +2,18 @@ var source = new EventSource("http://localhost:8144/quotes");
 source.onmessage = function(e){
   var data = JSON.parse(e.data);
   console.log(data);
+
+  var image = new Image();
+  image.onload = function() {
+     $(".content").fadeOut();
+     $(".content").css("background-image", "url(" + image.src + ")");
+     $(".content").fadeIn();
+
+     $("blockquote p").text(data.content);
+
+     $("cite").text(data.author);
+  };
+  image.src = data.picture;
+
 };
 
